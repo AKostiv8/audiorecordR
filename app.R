ec5a2ff34e271087c5413697a0418666de57f88a
AKostiv8 <nolik2000@yahoo.com>
2022-04-29 15:26:06 +0300

    update

--- app.R
+++ app.R
@@ -1,10 +1,47 @@
 library(shiny)
 library(audiorecordR)
 
-ui <- fluidPage(
-  titlePanel("Audio recorder / RShiny"),
-  # AudioReactRecorderOutput('widgetOutput')
-  AudioReactRecorder()
+# ui <- fluidPage(
+#
+#   tags$head(
+#     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+#   ),
+#
+#   titlePanel("Audio recorder / RShiny"),
+#   # AudioReactRecorderOutput('widgetOutput')
+#   div(class='row',
+#       div(class='box_one',
+#           AudioReactRecorder()
+#       ),
+#       div(class='box_twp'
+#
+#       )
+#   )
+# )
+ui <- div(class='container',
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "simple-grid.min.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
+    div(class='row',
+      div(class='col-2',
+          div(class='card')
+          ),
+      div(class='col-4',
+          div(class='card')
+          ),
+      div(class='col-6',
+          div(class='card')
+      )
+    ),
+    div(class='row',
+        div(class='col-6',
+            div(class='card')
+        ),
+        div(class='col-6',
+            div(class='card')
+        )
+    )
 )
 
 server <- function(input, output, session) {
